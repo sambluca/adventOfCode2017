0874ff981d89169edc15e7e740fc5647a42f693b
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-02 11:49:35 +0000

    day2 exercise 2 DONE

--- src/day2/day2.js
+++ src/day2/day2.js
@@ -2,7 +2,16 @@
 const exercise1 = (input) => 
     input.map((line) => Math.max(...line) - Math.min(...line))
     .reduce((acc, digit) => acc + digit, 0);
-export default { exercise1 }
 
+const exercise2 = (input) => 
+    input.map((line) => divideValue(line))
+    .reduce((acc, digit) => acc + digit);
 
-    
+const divideValue = (input) => input.reduce((acc, digit, index) => {
+    input.forEach((element, i) => {
+        acc = (digit%element === 0 && index !== i) ? digit/element : acc;
+    });
+    return acc;
+}, 0)
+
+export default { exercise1, exercise2 }
