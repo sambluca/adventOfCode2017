8cca0146b940b0f4f1bec1aa1ec620b7c8e60c19
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-02 11:49:40 +0000

    update test

--- test/day2.test.js
+++ test/day2.test.js
@@ -1,4 +1,4 @@
-import { exercise1 } from '../src/day2/day2';
+import { exercise1, exercise2 } from '../src/day2/day2';
 import data from '../data/day2';
 
 describe('exercise 1', ()=> {
@@ -10,6 +10,19 @@ describe('exercise 1', ()=> {
     test('input data', () => {
         const answer = exercise1(data);
         expect(answer).toEqual(44216);
+    });
+
+});
+
+describe('exercise 2', ()=> {
+    test('example data', () => {
+        const answer = exercise2([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]);
+        expect(answer).toEqual(9);
+    });
+
+    test('input data', () => {
+        const answer = exercise2(data);
+        expect(answer).toEqual(320);
     });
 
 });
