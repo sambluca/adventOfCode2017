5bd66989671c3c9763bcf5626a73abbec03c9cf5
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-02 01:15:45 +0000

    exercise 2

--- src/day1.js
+++ src/day1.js
@@ -1,6 +1,6 @@
 import template from '../src/day1';
 const intArray = (a = data) => a.split('').map(Number);
-const sequenceDigits = (sequence) => sequence.reduce((acc, digit, index) => {
+const sequenceDigits1 = (sequence) => sequence.reduce((acc, digit, index) => {
     const nextDigit = sequence[index+1];
         if((nextDigit === digit) || (index === (sequence.length - 1) && digit == sequence[0])) {            
             return acc + digit;
@@ -8,5 +8,18 @@ const sequenceDigits = (sequence) => sequence.reduce((acc, digit, index) => {
         return acc;
     }, 0);
 
-const exercise1 = sequence => sequenceDigits(intArray(sequence));
-export default { exercise1 }
+
+const sequenceDigits2 = (sequence) => sequence.reduce((acc, digit, index) => {
+    const nextDigit = sequence[index + sequence.length / 2] ;
+        if(nextDigit == digit || !nextDigit && digit == sequence[index - sequence.length / 2]) {
+            return acc + digit;
+        }
+        return acc;
+    }, 0);
+    
+
+const exercise1 = sequence => sequenceDigits1(intArray(sequence));
+
+const exercise2 = sequence => sequenceDigits2(intArray(sequence));
+
+export default { exercise1, exercise2 }
