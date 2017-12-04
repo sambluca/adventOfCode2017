1b667fee20cdc6e271e132d09673e78e258c51aa
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-04 11:34:22 +0000

    order alphabetically

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -3,7 +3,7 @@ import sys
 import os
 def read_file():
     wholeArray = []
-    with open("day3Data.txt", 'r') as f:
+    with open("day4Data.txt", 'r') as f:
         for line in f:
             lineArray = line.rstrip("\n").split(" ")
             wholeArray.append(lineArray)
@@ -23,4 +23,30 @@ def exercise1():
         valid = True
     print validCount
 
+def exercise2():
+    valid = True
+    validCount = 0
+    phraseArray = orderAlphabetically(read_file())
+    for phrase in phraseArray:
+        for word in phrase:
+            if phrase.count(word) != 1:
+                valid = False
+                break
+        if valid:
+            validCount = validCount + 1
+        valid = True
+    print validCount
+
+
+
+
+def orderAlphabetically(phraseArray):
+    for idx, phrase in enumerate(phraseArray):
+        for i, word in enumerate(phrase):
+            phrase[i] = ''.join(sorted(word))
+        phraseArray[idx] = phrase
+    return phraseArray
+
+
 exercise1()
+exercise2()
