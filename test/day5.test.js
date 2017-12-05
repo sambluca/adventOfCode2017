c2ac2daf4b55c66e425d6946cce01811095b5a00
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-05 11:45:26 +0000

    exercise 2 failing test

--- test/day5.test.js
+++ test/day5.test.js
@@ -1,4 +1,4 @@
-import { exercise1 } from '../src/day5/day5';
+import { exercise1, exercise2 } from '../src/day5/day5';
 import data from '../data/day5';
 
 describe('exercise 1', ()=> {
@@ -10,5 +10,17 @@ describe('exercise 1', ()=> {
     test('input data', () => {
         const answer = exercise1(data);
         expect(answer).toEqual(342669);
+    });
+});
+
+describe('exercise 2', ()=> {
+    test('example data', () => {
+        const answer = exercise2([0, 3, 0, 1, -3]);
+        expect(answer).toEqual(10);
+    });
+
+    test('input data', () => {
+        const answer = exercise2(data);
+        expect(answer).toEqual(77);
     });
 });
