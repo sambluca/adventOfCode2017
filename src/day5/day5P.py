9b2fc4074cff90a9c915355615b80a0cfc0adc02
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-05 11:53:34 +0000

    exercise2

--- src/day5/day5P.py
+++ src/day5/day5P.py
@@ -1,17 +1,34 @@
 # Basic File IO
 import sys
 import os
-wholeArray = []
-with open("day5.txt", 'r') as f:
-	for line in f:
-		lineArray = line.rstrip("\n")
-		wholeArray.append(int(float(lineArray)))
+def readfile():
+    wholeArray = []
+    with open("day5.txt", 'r') as f:
+        for line in f:
+            lineArray = line.rstrip("\n")
+            wholeArray.append(int(float(lineArray)))
+    return wholeArray
 
-steps = 0
-index = 0
-while(index >= 0 and index < len(wholeArray)):
-    wholeArray[index] += 1
-    index += wholeArray[index]
-    steps += 1
+def exercise1():
+    wholeArray = readfile()    
+    steps = 0
+    index = 0
+    while(index >= 0 and index < len(wholeArray)):
+        wholeArray[index] += 1
+        index += wholeArray[index]
+        steps += 1
+    print(steps)
 
-print(steps)
+def exercise2():
+    wholeArray = readfile()    
+    steps = 0
+    index = 0
+    while(index >= 0 and index < len(wholeArray)):
+        last = wholeArray[index]
+        wholeArray[index] += 1 if last < 3 else -1
+        index += last
+        steps += 1
+    print(steps)
+
+exercise1()
+exercise2()
