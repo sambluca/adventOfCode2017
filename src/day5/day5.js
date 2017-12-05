ce266521bce91e455bad2ebfa8cf998fe93b6ecb
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-05 11:57:38 +0000

    don't need exercise 2 done it in python

--- src/day5/day5.js
+++ src/day5/day5.js
@@ -8,17 +8,4 @@ const exercise1 = (input) => {
     return steps;
 };
 
-const exercise2 = (input) => {
-    var steps = 0;
-    var index = 0;
-    while(index >= 0 && index < input.length) {
-        index += input[index]++;
-        steps++;
-    }
-
-
-    return steps;
-};
-
-
-export default { exercise1, exercise2 }
+export default { exercise1 }
