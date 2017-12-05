61b5d3401a4e60bd5dba9228e5708b4a3eac36f4
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-05 11:17:05 +0000

    test with wrong answer

---
+++ test/day5.test.js
@@ -0,0 +1,14 @@
+import { exercise1 } from '../src/day5/day5';
+import data from '../data/day5';
+
+describe('exercise 1', ()=> {
+    test('example data', () => {
+        const answer = exercise1([0, 3, 0, 1, -3]);
+        expect(answer).toEqual(5);
+    });
+
+    test('input data', () => {
+        const answer = exercise1(data);
+        expect(answer).toEqual(44216);
+    });
+});
