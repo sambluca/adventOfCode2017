95f29da9f8b3cc3b4bd48ec288b9dba8a6425333
Gianluca Tramontana <gianluca.tramontana@bbc.co.uk>
2017-12-08 14:17:33 +0000

    exercise 2 and rename

--- src/day8/day8.py
+++ src/day8/day8.py
@@ -41,15 +41,18 @@ def runInstruction():
     registerNInstruction = readfile()
     registerDict = registerNInstruction[0]
     instructionDict = registerNInstruction[1]
-    
+    highestValue = 0
+    modifiedValue = 0
     for instruction in instructionDict:
         modify = modifyCheckValue(instructionDict[instruction]['instruction'], registerDict)
         if modify:
             registerValue = int(registerDict[instructionDict[instruction]['instructionToMod']])
             modifier = int(float(instructionDict[instruction]['modifyBy']))
             operation = instructionDict[instruction]['operation']
-            registerDict[instructionDict[instruction]['instructionToMod']] = operation(registerValue, modifier)
+            modifiedValue = operation(registerValue, modifier)
+            registerDict[instructionDict[instruction]['instructionToMod']] = modifiedValue
+        highestValue = modifiedValue if modifiedValue > highestValue else highestValue #exercise 2
     maxValue = registerDict[max(registerDict, key=registerDict.get)]
-
+    print highestValue
     return maxValue
 print runInstruction()
